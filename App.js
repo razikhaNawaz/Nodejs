e9165f54d84d0f7969d0734aec01df37f1110e3e
razikhaNawaz <razikhafaheem02@gmail.com>
2023-12-23 15:29:02 +0530

    completeRoute

--- App.js
+++ App.js
@@ -3,39 +3,40 @@ const http=require('http')
 const server=http.createServer((req,res)=>{
     
     const url = req.url;
-//   const method = req.method;
-  if (url === '/') {
-    res.write('<html>');
-    res.write('<head><title>Enter Message</title></head>');
-    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-    res.write('</html>');
-    res.end();
-  }else if(url=== '/home'){
-    res.write('<html>')
-    res.write('<head><title>Home page</title></head>')
-    res.write('<body><h1>Welcome to home Page</h1></body>')
-    res.write('</html>')
-    res.end();
-  }else if(url=== '/About'){
-    res.write('<html>')
-    res.write('<head><title>About page</title></head>')
-    res.write('<body><h1>Welcome to About us Page</h1></body>')
-    res.write('</html>')
-    res.end();
-  }else if(url=== '/node'){
-    res.write('<html>')
-    res.write('<head><title>Node page</title></head>')
-    res.write('<body><h1>Welcome to my node js Project</h1></body>')
-    res.write('</html>')
-    res.end();
-  }
-    res.setHeader('Content-Type', 'text/html')
-    res.write('<html>')
-    res.write('<head><title>My first page</title></head>')
-    res.write('<body><h1>Hello from my node.js server</h1></body>')
-    res.write('</html>')
-    res.end()
-  
+      const method = req.method;
+      if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+      }
+      if(url=== '/home'){
+        res.write('<html>')
+        res.write('<head><title>Home page</title></head>')
+        res.write('<body><h1>Welcome to home Page</h1></body>')
+        res.write('</html>')
+        return res.end();
+      }else if(url=== '/about'){
+        res.write('<html>')
+        res.write('<head><title>About page</title></head>')
+        res.write('<body><h1>Welcome to About us Page</h1></body>')
+        res.write('</html>')
+        return res.end();
+      }else if(url=== '/node'){
+        res.write('<html>')
+        res.write('<head><title>Node page</title></head>')
+        res.write('<body><h1>Welcome to my node js Project</h1></body>')
+        res.write('</html>')
+        return res.end();
+      }
+        res.setHeader('Content-Type', 'text/html')
+        res.write('<html>')
+        res.write('<head><title>My first page</title></head>')
+        res.write('<body><h1>Hello from my node.js server</h1></body>')
+        res.write('</html>')
+        res.end()
     
-})
-server.listen(3000)
+    
+    })
+    server.listen(4000)
